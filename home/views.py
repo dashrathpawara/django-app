f50949f23a62751303eb82c1059b074b2d111d6b
DashOps <dashrathpawara1997@gmail.com>
2024-07-30 18:27:36 +0530

    first commit after setting up the Django server

---
+++ home/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def home(request):
+    
+    peoples = [
+        {'name' : 'Ram Padvi' , 'age' : 28},
+        {'name' : 'Rohan Valvi' , 'age' : 27},
+        {'name' : 'Himmat Pawara' , 'age' : 25},
+        {'name' : 'Digesh Pawara' , 'age' : 1},
+    ]
+
+    return render(request , "home/index.html" , context= {'peoples' : peoples})
+
+
+
+
+def success_page(request):
+    print("*" * 10)
+    return HttpResponse(""" <h1> This is my KhapakhapDB </h1>
+    <p> must say that this is my second link to god
+    """)
