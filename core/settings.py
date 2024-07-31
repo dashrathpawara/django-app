475ec1ede3b49ee653a8f389f3e275287ce636bf
DashOps <dashrathpawara1997@gmail.com>
2024-07-31 20:01:55 +0530

    added some settings in app.py #6

--- core/settings.py
+++ core/settings.py
@@ -36,13 +36,12 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    
+    'django.contrib.staticfiles'  
 ]
 
 EXTERNAL_APPS = [
     'accounts',
-    'home'
+    'home.apps.HomeConfig'
 ]
 INSTALLED_APPS += EXTERNAL_APPS
 
