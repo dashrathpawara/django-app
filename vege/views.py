0ddd8e1a15bd2e81633b592eb7af6b0bb6f7f93a
DashOps <dashrathpawara1997@gmail.com>
2024-08-02 11:54:39 +0530

    Fixed the Search button

--- vege/views.py
+++ vege/views.py
@@ -17,13 +17,12 @@ def receipes(request):
             receipe_description = receipe_description
         )
         return redirect('/receipes/')
+    
     queryset = Receipe.objects.all()
 
     if request.GET.get('search'):
         queryset = queryset.filter(receipe_name__icontains = request.GET.get('search'))
-        
-
-
+    
     context = {'receipes' : queryset}
     return render (request , 'receipes.html', context)
 
